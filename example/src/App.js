53c8d23f524ebf7d24507aa82cfa32d269464c38
Samir Damle <damlemail@gmail.com>
2019-12-15 18:24:43 -0500

    Restarted work using create-react-hook

--- example/src/App.js
+++ example/src/App.js
@@ -2,14 +2,22 @@ import React from 'react'
 import {usePulse} from 'use-pulse'
 
 const App = () => {
-    const myPulse = usePulse(5, 11, 2, 500, 500)
+    const myPulse1 = usePulse(5, 11, 2, 500, 500)
+    const myPulse2 = usePulse(123, 456789, 234, 0, 345)
     return (
         <div>
-            <div>Pulse Value: {myPulse.value}</div>
+            <div>Pulse 1 Value: {myPulse1.value}</div>
             <div>
-                <button onClick={myPulse.reset}>Reset</button>
-                <button onClick={myPulse.start}>Start</button>
-                <button onClick={myPulse.stop}>Stop</button>
+                <button onClick={myPulse1.reset}>Reset</button>
+                <button onClick={myPulse1.start}>Start</button>
+                <button onClick={myPulse1.stop}>Stop</button>
+            </div>
+            <br/><br/>
+            <div>Pulse 2 Value: {myPulse2.value}</div>
+            <div>
+                <button onClick={myPulse2.reset}>Reset</button>
+                <button onClick={myPulse2.start}>Start</button>
+                <button onClick={myPulse2.stop}>Stop</button>
             </div>
         </div>
     )
